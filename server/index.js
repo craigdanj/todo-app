51129d942a066099884be50744837386cbe01973
Craig <mgi-ui@Prashants-MacBook-Pro.local>
2018-08-14 17:16:27 +0530

    sequelize and associated libs installed

---
+++ server/index.js
@@ -0,0 +1,103 @@
+
+const Sequelize = require('sequelize');
+const casual = require('casual');
+const _ = require('lodash');
+
+const db = new Sequelize('blog', null, null, {
+  dialect: 'sqlite',
+  storage: './blog.sqlite',
+});
+
+const AuthorModel = db.define('author', {
+  firstName: { type: Sequelize.STRING },
+  lastName: { type: Sequelize.STRING },
+});
+
+const PostModel = db.define('post', {
+  title: { type: Sequelize.STRING },
+  text: { type: Sequelize.STRING },
+});
+
+AuthorModel.hasMany(PostModel);
+PostModel.belongsTo(AuthorModel);
+
+// create mock data with a seed, so we always get the same
+casual.seed(123);
+db.sync({ force: true }).then(() => {
+  _.times(10, () => {
+    return AuthorModel.create({
+      firstName: casual.first_name,
+      lastName: casual.last_name,
+    }).then((author) => {
+      return author.createPost({
+        title: `A post by ${author.firstName}`,
+        text: casual.sentences(3),
+      });
+    });
+  });
+});
+
+const Author = db.models.author;
+const Post = db.models.post;
+
+
+
+//========================================
+//===========    SERVER    ===============
+//========================================
+
+
+const { ApolloServer, gql } = require('apollo-server');
+
+// This is a (sample) collection of books we'll be able to query
+// the GraphQL server for.  A more complete example might fetch
+// from an existing data source like a REST API or database.
+const books = [
+  {
+    title: 'Moby Dick',
+    author: 'H Melville',
+  },
+  {
+    title: 'Jurassic Park',
+    author: 'Michael Crichton',
+  },
+];
+
+// Type definitions define the "shape" of your data and specify
+// which ways the data can be fetched from the GraphQL server.
+const typeDefs = gql`
+  # Comments in GraphQL are defined with the hash (#) symbol.
+
+  # This "Book" type can be used in other type declarations.
+  type Book {
+    title: String
+    author: String
+  }
+
+  # The "Query" type is the root of all GraphQL queries.
+  # (A "Mutation" type will be covered later on.)
+  type Query {
+    books: [Book]
+  }
+`;
+
+// Resolvers define the technique for fetching the types in the
+// schema.  We'll retrieve books from the "books" array above.
+const resolvers = {
+  Query: {
+    books: () => books,
+  },
+};
+
+// In the most basic sense, the ApolloServer can be started
+// by passing type definitions (typeDefs) and the resolvers
+// responsible for fetching the data for those types.
+const server = new ApolloServer({ typeDefs, resolvers });
+
+// This `listen` method launches a web-server.  Existing apps
+// can utilize middleware options, which we'll discuss later.
+server.listen().then(({ url }) => {
+  console.log(`🚀  Server ready at ${url}`);
+});
+
+
