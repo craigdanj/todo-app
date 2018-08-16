08d6f7ceffdc02989df967f91cc590e513b39a23
Craig <craigdanj@gmail.com>
2018-08-16 17:15:45 +0530

    style fo todo list added

--- client/src/components/list/index.js
+++ client/src/components/list/index.js
@@ -34,6 +34,18 @@ export default class Home extends Component {
                     <input type="text" value={this.state.todoText} onChange={this.handleOnInputChange}/>
                     <button onClick={this.handleAddTodo}>Add</button>
                 </div>
+
+                <ul class={style.todolist}>
+                    <li>
+                        <span>aaa</span>
+                    </li>
+                    <li>
+                        <span>bb</span>
+                    </li>
+                    <li>
+                        <span>ccc</span>
+                    </li>
+                </ul>
             </div>
         )
     }
