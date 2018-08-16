7a1684bfe4ffcd7730fcf3543539d32a2ca4fc76
Craig <craigdanj@gmail.com>
2018-08-16 16:49:30 +0530

    header links remoevd

--- client/src/components/list/index.js
+++ client/src/components/list/index.js
@@ -1,11 +1,14 @@
-import { h } from 'preact';
+import { h, Component } from 'preact';
 import style from './style';
 
 const Home = () => (
 	<div class={style.home}>
 		<h1>Todo list</h1>
-        <input type="text"/>
-        <button>Add</button>
+
+        <div>
+            <input type="text"/>
+            <button>Add</button>
+        </div>
 	</div>
 );
 
